c8e3915ae6397e64b73a10c0869f98d32b1cbaea
rf-ferreira <r4fael.s0uza@gmail.com>
2020-10-27 07:39:39 -0300

    pomodoro timer

---
+++ Pomodoro Timer/Pomodoro Timer/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Pomodoro_Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.setTime = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.setTime)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(51, 160);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer
+            // 
+            this.timer.AutoSize = true;
+            this.timer.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timer.Location = new System.Drawing.Point(33, 23);
+            this.timer.Name = "timer";
+            this.timer.Size = new System.Drawing.Size(289, 108);
+            this.timer.TabIndex = 2;
+            this.timer.Text = "25:00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(54, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Minutes";
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(150, 160);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 4;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(247, 160);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // setTime
+            // 
+            this.setTime.Location = new System.Drawing.Point(57, 216);
+            this.setTime.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.setTime.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.setTime.Name = "setTime";
+            this.setTime.Size = new System.Drawing.Size(69, 20);
+            this.setTime.TabIndex = 7;
+            this.setTime.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(363, 265);
+            this.Controls.Add(this.setTime);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.timer);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Pomodoro Timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.setTime)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label timer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown setTime;
+    }
+}
+
---
+++ Pomodoro Timer/Pomodoro Timer/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Pomodoro_Timer
+{
+    public partial class Form1 : Form
+    {
+        int time, minute, second;
+        bool running = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            running = false;
+            Console.WriteLine("tedsadas");
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            minute = Convert.ToInt32(setTime.Text);
+            second = 0;
+            if (minute < 10)
+            {
+                timer.Text = "0" + minute + ":00";
+            }
+            else
+            {
+                timer.Text = minute + ":00";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            second--;
+            if (minute > 0)
+            {
+                if (second < 0)
+                {
+                    second = 59;
+                    minute--;
+                }
+            }
+            if (minute < 10 && second < 10)
+            {
+                timer.Text = "0" + minute + ":" + "0" + second;
+            }
+            else if (minute >= 10 && second < 10)
+            {
+                timer.Text = minute + ":" + "0" + second;
+            }
+            else if (minute < 10 && second >= 10)
+            {
+                timer.Text = "0" + minute + ":" + second;
+            }
+            else
+            {
+                timer.Text = minute + ":" + second;
+            }
+            if (minute == 0 && second == 0)
+            {
+                timer1.Enabled = false;
+                //SoundPlayer alarmSound = new SoundPlayer(@"Alarm\Clock\Path");
+                //alarmSound.Play();
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int newTime = Convert.ToInt32(setTime.Text);
+            if (newTime != time)
+            {
+                running = true;
+            }
+            if (running)
+            {
+                time = Convert.ToInt32(setTime.Text);
+                minute = time;
+                second = 0;
+                if (minute < 10)
+                {
+                    timer.Text = "0" + minute + ":00";
+                }
+                else
+                {
+                    timer.Text = minute + ":00";
+                }
+
+                timer1.Enabled = true;
+            }
+            else
+            {
+                if (minute < 10 && second < 10)
+                {
+                    timer.Text = "0" + minute + ":" + "0" + second;
+                }
+                else if (minute >= 10 && second < 10)
+                {
+                    timer.Text = minute + ":" + "0" + second;
+                }
+                else if (minute < 10 && second >= 10)
+                {
+                    timer.Text = "0" + minute + ":" + second;
+                }
+                else
+                {
+                    timer.Text = minute + ":" + second;
+                }
+                timer1.Enabled = true;
+                running = true;
+
+            }
+        }
+    }
+}
